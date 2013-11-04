992122c11c1c3934326a5839651726b3f129331b
kstradley <kylie.stradley@gmail.com>
2013-11-04 13:26:15 -0500

    update issues from first try
    addressed hidden field

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = '4ff4fe1e3b453d819e5db11da60f9f61df7929b58c866b6488e0f3279d621102f63e5e18ceabd6fd7641d7b704a45c2a1b935a4d582d0a4a60b4b74c288de2d2'
